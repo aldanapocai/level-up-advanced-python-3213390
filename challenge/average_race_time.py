b76cc1d0095d9483a6e3ee7724b47466fe0205a0
Aldana Pocai <38792460+aldanapocai@users.noreply.github.com>
2022-11-30 00:09:56 +0000

    challenge 1 completed. There is a bug though.

--- challenge/average_race_time.py
+++ challenge/average_race_time.py
@@ -4,16 +4,27 @@
 import re
 import datetime
 
+
 def get_data():
     """Return content from the 10k_racetimes.txt file"""
     with open('10k_racetimes.txt', 'rt') as file:
         content = file.read()
     return content
 
+
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
     races = get_data()
-    pass
+    rhines_times = []
+
+    def get_time(line):
+        return re.findall(r'\d{2}:\S+', line)[0]
+
+    for line in races.splitlines():
+        if "Jennifer Rhines" in line:
+            rhines_times.append(get_time(line))
+    return rhines_times
+
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
@@ -22,4 +33,16 @@ def get_average():
        s corresponds to a seconds digit
        M corresponds to a milliseconds digit (no rounding, just the single digit)"""
     racetimes = get_rhines_times()
-    pass
+    total = datetime.timedelta()
+    for racetime in racetimes:
+        try:
+            mins, secs, ms = re.split(r'[:.]', racetime)
+            total += datetime.timedelta(minutes=int(mins),
+                                        seconds=int(secs), milliseconds=int(ms))
+        except ValueError:
+            mins, secs = re.split(r'[:.]', racetime)
+            total += datetime.timedelta(minutes=int(mins), seconds=int(secs))
+
+        return f'{total / len(racetimes)}'[2:-5]
+
+    # Issue with Total
